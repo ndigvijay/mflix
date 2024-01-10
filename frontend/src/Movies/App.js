c60d6b6f6143c6f4013a978403cf247072165c63
ndigvijay <digvijayn1993@gmail.com>
2024-01-10 07:09:55 +0530

    added spiderman as default search for omdb

--- frontend/src/Movies/App.js
+++ frontend/src/Movies/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies();
+    searchMovies("spiderman");
   }, []);
 
   const searchMovies = async (title) => {
